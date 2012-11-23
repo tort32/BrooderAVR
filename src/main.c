205c58e1149772ccea4ad304b2950d1de0e06e9c
TORT <tort.spb@gmail.com>
2012-11-24 02:45:36 +0400

    define pins

--- src/main.c
+++ src/main.c
@@ -4,37 +4,25 @@
 #include <util/delay.h>
 
 /* ----------------------- hardware I/O abstraction ------------------------ */
-/*#define KEY1 _BV(PB0)
-#define KEY2 _BV(PB1)
-#define KEY3 _BV(PB2)
-#define KEY4 _BV(PB3)
-#define KEY5 _BV(PB4)
-#define KEY6 _BV(PB5)
-#define KEY7 _BV(PB6)
-#define KEY8 _BV(PB7)
+#define TEMP_1WIRE _BV(PB0)
 
-#define KEY9 _BV(0)
-#define KEY10 _BV(1)
-#define KEY11 _BV(2)
-#define KEY12 _BV(3)
-#define KEY13 _BV(4)
-#define KEY14 _BV(5)
-#define KEY15 _BV(6)
-#define KEY16 _BV(7)
+#define TIME_SCL _BV(PD0)
+#define TIME_SDA _BV(PD1)
 
-#define KEYA _BV(PD0)
-#define KEYB _BV(PD6)
+#define LCD_D4 _BV(PC0)
+#define LCD_D5 _BV(PC1)
+#define LCD_D6 _BV(PC2)
+#define LCD_D7 _BV(PC3)
+#define LCD_RS _BV(PC4)
+#define LCD_E _BV(PC5)
 
-#define LED1 _BV(PD3)
-#define LED2 _BV(PD4)
-#define LED3 _BV(PD5)
-
-#define PORTB_KEY (KEY1|KEY2|KEY3|KEY4|KEY5|KEY6|KEY7|KEY8)
-#define PORTD_KEY (KEYA|KEYB)
-#define PORTD_LED (LED1|LED2|LED3)*/
+#define TEMP_PORT PORTB
+#define TIME_PORT PORTD
+#define LCD_PORT PORTC
 
 static void hardwareInit(void)
 {
+
   /*DDRB=0;
   PORTB=PORTB_KEY;
   DDRD=PORTD_LED;
